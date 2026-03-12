06a21f2e53993757162dff8254f99224578a6c2a
mw2000 <mwadekar2000@gmail.com>
2026-03-11 21:33:14 -0700

    Fix call frame test formatting

--- test/call_frame_test.exs
+++ test/call_frame_test.exs
@@ -30,6 +30,7 @@ defmodule EEVM.CallFrameTest do
 
   test "push_frame enforces max call depth of 1024" do
     state = MachineState.new(<<0x00>>, depth: 1024)
+
     frame = %CallFrame{
       code: <<0x00>>,
       stack: Stack.new(),
