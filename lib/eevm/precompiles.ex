16de20c0cedb7ec4448c29bcabf230d474ca55b5
Mihir Wadekar <mwadekar2000@gmail.com>
2026-03-16 12:13:00 -0700

    Add precompile routing in CALL path

---
+++ lib/eevm/precompiles.ex
@@ -0,0 +1,17 @@
+defmodule EEVM.Precompiles do
+  @moduledoc false
+
+  @spec is_precompile?(non_neg_integer()) :: boolean()
+  def is_precompile?(address) when address >= 0x01 and address <= 0x0A, do: true
+  def is_precompile?(_), do: false
+
+  @spec execute(non_neg_integer(), binary(), non_neg_integer()) ::
+          {:ok, binary(), non_neg_integer()} | {:error, atom()}
+  def execute(address, input, gas_limit) do
+    :erlang.apply(__MODULE__, :do_execute, [address, input, gas_limit])
+  end
+
+  def do_execute(_address, _input, _gas_limit) do
+    {:error, :not_implemented}
+  end
+end
