48d008485c84c916aa6677706002745508f89f9b
Mihir Wadekar <mwadekar2000@gmail.com>
2026-04-23 00:01:27 -0700

    Implement set-code transactions (EIP-7702) (#106)
    feat(tx): add set-code transaction structs
    
    Introduce the Prague set-code transaction data model so authorization tuples and delegated execution metadata have a stable representation throughout the VM.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- lib/eevm/transaction.ex
+++ lib/eevm/transaction.ex
@@ -16,6 +16,24 @@ defmodule EEVM.Transaction do
           | EEVM.Transaction.AccessList.t()
           | EEVM.Transaction.FeeMarket.t()
           | EEVM.Transaction.Blob.t()
+          | EEVM.Transaction.SetCode.t()
+
+  @type authorization_tuple :: EEVM.Transaction.Authorization.t()
+
+  defmodule Authorization do
+    @moduledoc "EIP-7702 authorization tuple."
+
+    @type t :: %__MODULE__{
+            chain_id: non_neg_integer(),
+            address: EEVM.Transaction.address(),
+            nonce: non_neg_integer(),
+            y_parity: 0 | 1,
+            r: non_neg_integer(),
+            s: non_neg_integer()
+          }
+
+    defstruct [:chain_id, :address, :nonce, :y_parity, :r, :s]
+  end
 
   defmodule Legacy do
     @moduledoc "Legacy transaction (type 0)."
@@ -133,6 +151,42 @@ defmodule EEVM.Transaction do
       :access_list,
       :max_fee_per_blob_gas,
       :blob_versioned_hashes,
+      :y_parity,
+      :r,
+      :s
+    ]
+  end
+
+  defmodule SetCode do
+    @moduledoc "EIP-7702 set-code transaction (type 4)."
+
+    @type t :: %__MODULE__{
+            chain_id: non_neg_integer(),
+            nonce: non_neg_integer(),
+            max_priority_fee_per_gas: non_neg_integer(),
+            max_fee_per_gas: non_neg_integer(),
+            gas_limit: non_neg_integer(),
+            to: EEVM.Transaction.address(),
+            value: non_neg_integer(),
+            data: binary(),
+            access_list: EEVM.Transaction.access_list(),
+            authorization_list: [EEVM.Transaction.authorization_tuple()],
+            y_parity: 0 | 1,
+            r: non_neg_integer(),
+            s: non_neg_integer()
+          }
+
+    defstruct [
+      :chain_id,
+      :nonce,
+      :max_priority_fee_per_gas,
+      :max_fee_per_gas,
+      :gas_limit,
+      :to,
+      :value,
+      :data,
+      :access_list,
+      :authorization_list,
       :y_parity,
       :r,
       :s
